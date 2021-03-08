50e5356692b8186e21135a4d0ea649afc5ba2465
Edward Kunkel <edward.pk94@gmail.com>
2021-03-07 19:43:05 -0500

    Adding data, initial code, and requirements file

---
+++ BertDisneyReviews.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import torch
+import torch.nn
+import torch.nn.functional
+import torch.optim
+
+class BertDisneyReviews(torch.nn.Module):
+    def __init__(self):
+        super(BertDisneyReviews, self).__init__()
+
+    def forward(self):
+        pass
+
+def train():
+    pass
+
+def main():
+    BertDisneyReviews()
+    print('hello world!')
+
+if __name__ == "__main__":
+    main()
