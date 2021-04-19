f5914c8f5590392c25fc1f79724c3188793738aa
Edward Kunkel <edward.pk94@gmail.com>
2021-04-18 22:03:09 -0400

    Changed model to be binary classification sentiment model

--- BertDisneyReviews.py
+++ BertDisneyReviews.py
@@ -1,4 +1,6 @@
+from collections import Counter
 import pandas as pd
+import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from pathlib import Path
@@ -13,12 +15,18 @@ from transformers import AutoModel, AutoTokenizer, BertForSequenceClassification
 
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import classification_report
 
 import pdb
 
 class DisneyReviewsDataset(Dataset):
     def __init__(self, in_file, bert_model_name, debug=False, num_debug_rows=None):
         self.bert_model_name = bert_model_name
+
+        # Which sentiment analysis is being performed.
+        # Regression from 1-5 was not effective 
+        self.sentiment_type = 'binary'
+
         in_file_ob = Path(in_file)
         if in_file_ob.suffix == '.pkl':
             # Pickle file already contains tokens
@@ -32,8 +40,10 @@ class DisneyReviewsDataset(Dataset):
             if debug:
                 df = df.iloc[0:num_debug_rows]
 
-            # Normalize rating by dividing by max
-            df['Rating'] = df['Rating'].astype(float) / 5
+            # Remove neutral ratings (3/5)
+            df = df[df['Rating'] != 3]
+            df['Binary_Rating'] = df['Rating'].apply(lambda x: 1 if x > 3 else 0)
+
             print('Tokenizing model...')
             tokenizer = AutoTokenizer.from_pretrained(self.bert_model_name, do_lower_case=True)
             self.encodings = {}
@@ -57,7 +67,7 @@ class DisneyReviewsDataset(Dataset):
 
                     pbar.update(chunksize)
 
-            self.ratings = df['Rating'].tolist()
+            self.rating = df['Binary_Rating'].tolist()
 
     def __chunker(self, seq, size):
         """
@@ -66,14 +76,14 @@ class DisneyReviewsDataset(Dataset):
         return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
     def __len__(self):
-        return len(self.ratings)
+        return len(self.rating)
 
     def __getitem__(self, index):
         """
         https://huggingface.co/transformers/custom_datasets.html#seq-imdb
         """
         item = {key: torch.tensor(val[index]) for key, val in self.encodings.items()}
-        item['ratings'] = torch.tensor(self.ratings[index])
+        item['rating'] = torch.tensor(self.rating[index])
         return item
 
 class BertDisneyReviews(torch.nn.Module):
@@ -84,26 +94,26 @@ class BertDisneyReviews(torch.nn.Module):
 
         # Create the model. Output label is a single scalar (the review score)
         self.model = AutoModel.from_pretrained(model_name)
-
         self.drop_out = nn.Dropout(p=0.3)
         self.output = nn.Linear(self.model.config.hidden_size, self.num_outputs)
 
     def forward(self, input_ids, attention_mask):
-        _, pooled_output = self.model(input_ids, attention_mask)
+        _, pooled_output = self.model(input_ids=input_ids, attention_mask=attention_mask)
         x = self.drop_out(pooled_output)
         return self.output(x)
 
 def main():
-    debug = False
+    debug = True
     num_debug_rows = 100
-    batch_size = 1
+    batch_size = 4
     num_workers = 8
-    NUM_EPOCHES = 20
+    NUM_EPOCHES = 1
     learning_rate = 0.0001
-    PRINT_FREQUENCY = 200
+    PRINT_FREQUENCY = 20
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    # device = 'cpu'
+    optimizer_alg = 'Adam'
     model_name = 'bert-base-uncased'
-    model_name = 'distilbert-base-uncased'
     in_file = './DisneylandReviews.csv'
 
     dataset = DisneyReviewsDataset(in_file, model_name, debug=debug, num_debug_rows=num_debug_rows)
@@ -111,6 +121,18 @@ def main():
     # Fix any small errors due to float multiplication and rounding. The splits need to add up to the full dataset
     dataset_split_lengths[0] = dataset_split_lengths[0] + (len(dataset) - dataset_split_lengths[0] - dataset_split_lengths[1] - dataset_split_lengths[2])
     train_dataset, validation_dataset, test_dataset = torch.utils.data.random_split(dataset, dataset_split_lengths)
+
+    # Weights to fix large class imbalance between positive and negative reviews
+    #    (There are many more positive reviews than negative)
+    train_classes = [dataset.rating[i] for i in train_dataset.indices]
+    rating_counts = Counter(train_classes)
+    weights = []
+    num_train_samples = len(train_dataset)
+    for label, count in rating_counts.items():
+        # weight = max(rating_counts.values()) / count
+        weight = (num_train_samples - count) / count
+        weights.append(weight)
+    weight = torch.tensor(weights[0])
 
     train_loader = torch.utils.data.DataLoader(train_dataset,
                 batch_size=batch_size,
@@ -123,30 +145,34 @@ def main():
     test_loader = torch.utils.data.DataLoader(test_dataset,
                 batch_size=batch_size,
                 num_workers=num_workers)
-                
+
     model = BertDisneyReviews(device, model_name)
     # model = BertForSequenceClassification.from_pretrained(model_name)
     model.train()
     model.to(device)
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    # optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
-    loss_func = torch.nn.MSELoss()
+    if optimizer_alg == 'Adam':
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    elif optimizer_alg == 'SGD':
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    else:
+        print('incorrect optimizer_alg specified')
+        sys.exit(1)
+
+    loss_func = torch.nn.BCEWithLogitsLoss(pos_weight=weight).to(device)
     print('\nstarting training loop...')
     loss_history = []
-    # Saves for which mini batches a loss was saved
     loss_history_mini_batch_count = []
     for epoch in range(NUM_EPOCHES):
         print('starting epoch {}'.format(epoch))
         running_loss = 0.0
         num_mini_batches = len(train_loader)
         for count, data in enumerate(train_loader):
-            input_ids, ratings, attention_mask = data['input_ids'].to(device), data['ratings'].to(device), data['attention_mask'].to(device)
+            input_ids, rating, attention_mask = data['input_ids'].to(device), data['rating'].to(device), data['attention_mask'].to(device)
             outputs = model(input_ids=input_ids, attention_mask=attention_mask)
             optimizer.zero_grad()
             outputs = torch.squeeze(outputs)
-            loss = loss_func(outputs, ratings)
+            loss = loss_func(outputs, rating.type_as(outputs))
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
@@ -159,25 +185,29 @@ def main():
                 running_loss = 0.0
 
     print('testing the model...')
+    y_test_np = np.empty(1)
+    y_pred_np = np.empty(1)
     with torch.no_grad():
-        running_mse = torch.tensor(0.0)
-        running_mae = torch.tensor(0.0)
+        model.eval()
+        running_loss = torch.tensor(0.0)
         for count, data in enumerate(test_loader):
-            input_ids, ratings, attention_mask = data['input_ids'].to(device), data['ratings'].to(device), data['attention_mask'].to(device)
+            input_ids, y_test, attention_mask = data['input_ids'].to(device), data['rating'].to(device), data['attention_mask'].to(device)
             outputs = model(input_ids=input_ids, attention_mask=attention_mask).squeeze()
-            running_mse = torch.add(running_mse, torch.sum(torch.pow(torch.subtract(outputs, ratings), 2)))
-            running_mae = torch.add(running_mae, torch.sum(torch.abs(torch.subtract(outputs, ratings))))
-        
-        total_mse = running_mse.cpu().item() / len(test_dataset)
-        total_mae = running_mae.cpu().item() / len(test_dataset)
-        print('total_mse = {}\ntotal_mae = {}'.format(total_mse, total_mae))
+            y_pred = torch.round(torch.sigmoid(outputs))
+            
+            if len(y_test_np) > 1:
+                y_test_np = y_test.to('cpu').numpy()
+                y_pred_np = y_pred.to('cpu').numpy()
+            else:
+                y_test_np = np.concatenate((y_test_np, y_test.to('cpu').numpy()), axis=None)
+                y_pred_np = np.concatenate((y_pred_np, y_pred.to('cpu').numpy()), axis=None)
 
+    print(classification_report(y_test_np, y_pred_np))
     fig, ax = plt.subplots()
     ax.plot(loss_history_mini_batch_count, loss_history)
-    ax.set(xlabel='Mini Batch Number', ylabel='Mean Squared Error', title='Loss Versus Iterations')
+    ax.set(xlabel='Mini Batch Number', ylabel='Loss', title='Loss Versus Iterations')
     plt.show()
     pdb.set_trace()  
-
 
 if __name__ == "__main__":
     main()
